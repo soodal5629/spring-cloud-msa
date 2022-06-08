e31bca1d10a7c13778ea961d2890774d5211a15d
chaelynjang <chaelynjang@lotte.net>
2022-06-08 17:48:59 +0900

    yml 파일로 필터 적용

--- apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -5,12 +5,12 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
     /*
     * application.yml에 라우팅 정보를 java 코드로 구현한 것
     * */
-    @Bean
+    //@Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
